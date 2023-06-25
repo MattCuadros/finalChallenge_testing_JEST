2beabff2e1a2c62963069319cb4ab2ddaa3549fc
Matt Cuadros <mcuadrose@gmail.com>
2023-06-25 16:06:08 -0400

    README.md added

---
+++ README.md
@@ -0,0 +1,17 @@
+> # Challenge #Final TEST - **Cafe Nanacau - TESTING - JEST - SUPERTEST**
+
+---
+
+In this Challenge, i used JEST and SUPERTEST for make unitary tests. This tests are used into a server thats implements CRUD operations for cafes.json.
+
+---
+
+###### Contact me in:
+
+> - :bust_in_silhouette: Matias Cuadros
+>   -:telephone_receiver: +569 4154 9653
+>   -:email: <a href="mailto:mcuadrose@gmail.com" target="_blank">mcuadrose@gmail.com</a>
+
+---
+
+> ###### :warning: _All Rights Reserved - Visit my :briefcase: Briefcase in_ <a href="https://mattcuadros.github.io/" target="_blank">https://mattcuadros.github.io/</a> :copyright:
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,35 +1,77 @@
+// Importar el módulo "supertest" para realizar las pruebas HTTP
 const request = require("supertest");
+
+// Importar el servidor que se va a probar
 const server = require("../index");
 
+// Descripción de las pruebas
 describe("Operaciones CRUD de cafes", () => {
+  
+  // Prueba para la ruta GET/cafes
   it("Probando que la ruta GET/cafes devuelve status 200 y el tipo de dato es un arreglo con al menos 1 objeto", async () => {
+    // Realizar una solicitud GET a la ruta /cafes
     const response = await request(server).get("/cafes").send();
+    
+    // Obtener el código de estado de la respuesta
     const code = response.statusCode;
+    
+    // Obtener el cuerpo de la respuesta (en este caso, un arreglo de objetos)
     const cafes = response.body;
+    
+    // Verificar que el código de estado sea 200
     expect(code).toBe(200);
+    
+    // Verificar que la variable "cafes" sea un arreglo
     expect(cafes).toBeInstanceOf(Array);
+    
+    // Verificar que el arreglo "cafes" tenga al menos un objeto
     expect(cafes.length).toBeGreaterThanOrEqual(1);
   });
+
+  // Prueba para la ruta DELETE/cafes/:id
   it("Probando que se obtiene un status 404, cuando se intenta eliminar un cafe con un id que no existe", async () => {
+    // Definir un token de autorización (jwt) y un id de cafe para eliminar
     const jwt = "token";
     const idCafeEliminar = 5;
+    
+    // Realizar una solicitud DELETE a la ruta /cafes/:id con el token de autorización y el id del cafe
     const response = await request(server)
       .delete(`/cafes/${idCafeEliminar}`)
       .set("Authorization", jwt)
       .send();
+    
+    // Verificar que el código de estado de la respuesta sea 404
     expect(response.statusCode).toBe(404);
   });
+
+  // Prueba para la ruta POST/cafes
   it("Probando la ruta POST/cafes agrega un nuevo cafe y devuelve código 201", async () => {
+    // Definir un nuevo cafe (con un id único y un nombre)
     const nuevoCafe = { id: Date.now(), nombre: "Lungo" };
+    
+    // Realizar una solicitud POST a la ruta /cafes con el cuerpo del nuevo cafe
     const response = await request(server).post(`/cafes`).send(nuevoCafe);
-    console.log(response.body);
-    console.log(response.statusCode);
+    
+    // Imprimir en la consola el cuerpo de la respuesta y el código de estado
+    /* console.log(response.body);
+    console.log(response.statusCode); */
+    
+    // Verificar que el código de estado de la respuesta sea 201
     expect(response.statusCode).toBe(201);
   });
+
+  // Prueba para la ruta PUT/cafes/:id
   it("Probando la ruta PUT/cafes devuelve status 400 si no coincide el id del req.params con el id enviado en el body", async () => {
+    // Definir un cafe a editar (con un id que no coincide con el id en la ruta y un nombre)
     const editCafe = { id: 3, nombre: "Lungo" };
-    const {body, statusCode} = await request(server).put(`/cafes/2`).send(editCafe);
-    console.log('El mensaje recibido es ',body.message);
+    
+    // Realizar una solicitud PUT a la ruta /cafes/:id con el cuerpo del cafe editado
+    const { body, statusCode } = await request(server).put(`/cafes/2`).send(editCafe);
+    
+    // Imprimir en la consola el mensaje recibido en el cuerpo de la respuesta
+    console.log('El mensaje recibido es ', body.message);
+    
+    // Verificar que el código de estado de la respuesta sea 400
     expect(statusCode).toBe(400);
   });
 });
