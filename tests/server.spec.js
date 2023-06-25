71db913ced15c459fee2cec72f760a8c646904fd
Matt Cuadros <mcuadrose@gmail.com>
2023-06-25 15:41:08 -0400

    Second Test added

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -4,11 +4,16 @@ const server = require("../index");
 describe("Operaciones CRUD de cafes", () => {
   it("Probando que la ruta GET/cafes devuelve status 200 y el tipo de dato es un arreglo con al menos 1 objeto", async () => {
     const response = await request(server).get("/cafes").send();
-    console.log(response);
     const code = response.statusCode;
     const cafes = response.body;
-    console.log(cafes);
     expect(code).toBe(200);
     expect(cafes).toBeInstanceOf(Array);
+    expect(cafes.length).toBeGreaterThanOrEqual(1);
+  });
+  it("Probando que se obtiene un status 404, cuando se intenta eliminar un cafe con un id que no existe", async () => {
+    const jwt="token"
+    const idCafeEliminar=5;
+    const response = await request(server).delete(`/cafes/${idCafeEliminar}`).set("Authorization",jwt).send();
+    expect(response.statusCode).toBe(404);
   });
 });
